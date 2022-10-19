5da451da1882d8a61528b788b7637023a65a3108
cs19b014iittp <70994761+cs19b014iittp@users.noreply.github.com>
2022-10-19 11:39:43 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -9,13 +9,8 @@ def kali():
 class cs19b014NN(nn.Module, model_type, loader_size, classes, config=None):
   def __init__(self):
     super().__init__()
-
+    self.classes = classes
     self.linear_relu_stack = nn.Sequential(
-      # nn.Linear(28*28, 512),
-      # nn.ReLU(),
-      # nn.Linear(512, 512),
-      # nn.ReLU(),
-      # nn.Linear(512, 10),
       if model_type === 1:
         for i in range(len(config)):
             nn.Conv2d(config[i][0], config[i][1], config[i][2], stride=config[i][3], padding=config[i][4])
@@ -28,18 +23,14 @@ class cs19b014NN(nn.Module, model_type, loader_size, classes, config=None):
         nn.Conv2d(16, 50, 5)
         nn.ReLu()
         nn.MaxPool2d(2,2)
-        nn.Linear(28*28, 512),
-        # nn.ReLU(),
-        # nn.Linear(512, 512),
-        # nn.ReLU(),
+        
     )
 
     def forward(self, x):
         logits = self.linear_relu_stack(x)
         x = torch.flatten(x, 1)
-        x = self.fc3(x)
+        x = F.relu(nn.Linear(len(x), self.classess))
         return x
-
 
   # ... your code ...
   # ... write init and forward functions appropriately ...
@@ -54,7 +45,21 @@ def get_model(train_data_loader=None, n_epochs=10):
     break
   model = cs19b014NN(0, loader_size, classes)
 
+  loss_fn = nn.CrossEntropyLoss()
+  optimizer = torch.optim.SGD(model.parameters())
 
+  for epoch in range(n_epochs):
+    for batch, (X, y) in enumerate(train_data_loader):
+        # Compute prediction and loss
+        pred_ = model(X)
+        softmax = nn.Softmax(dim=1)
+        pred = softmax(pred_)
+        loss = loss_fn(pred, y)
+
+        # Backpropagation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
 
   # write your code here as per instructions
   # ... your code ...
@@ -63,13 +68,35 @@ def get_model(train_data_loader=None, n_epochs=10):
   # Use softmax and cross entropy loss functions
   # set model variable to proper object, make use of train_data
   
-  print ('Returning model... (rollnumber: xx)')
+  print ('Returning model... (rollnumber: 14)')
   
   return model
 
 # sample invocation torch.hub.load(myrepo,'get_model_advanced',train_data_loader=train_data_loader,n_epochs=5, force_reload=True)
 def get_model_advanced(train_data_loader=None, n_epochs=10,lr=1e-4,config=None):
-  model = None
+  train_features, train_labels = next(iter(train_data_loader))
+  loader_size = train_features.size()
+  classes=None
+  for X, y in dataloader:
+    classes = len(y)
+    break
+  model = cs19b014NN(1, loader_size, classes, config=config)
+
+  loss_fn = nn.CrossEntropyLoss()
+  optimizer = torch.optim.SGD(model.parameters())
+
+  for epoch in range(n_epochs):
+    for batch, (X, y) in enumerate(train_data_loader):
+        # Compute prediction and loss
+        pred_ = model(X)
+        softmax = nn.Softmax(dim=1)
+        pred = softmax(pred_)
+        loss = loss_fn(pred, y)
+
+        # Backpropagation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
 
   # write your code here as per instructions
   # ... your code ...
@@ -87,10 +114,10 @@ def get_model_advanced(train_data_loader=None, n_epochs=10,lr=1e-4,config=None):
   
   # HINT: You can print sizes of tensors to get an idea of the size of the fc layer required
   # HINT: Flatten function can also be used if required
-  return model
+  # return model
   
   
-  print ('Returning model... (rollnumber: xx)')
+  print ('Returning model... (rollnumber: 14)')
   
   return model
 
